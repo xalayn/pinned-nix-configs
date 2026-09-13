f9e9c62bcffc4f0a35e74116e683496c158dd01a
Alex Kumpula <alex.kumpula01@gmail.com>
2026-09-12 18:06:55 -0600

    update

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Common flake to unify nixpkgs for system + home";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     system.url = "github:alex-kumpula/system-config";
